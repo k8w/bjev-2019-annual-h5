02e365dba4d126efd22de949a4c6f098b9c065a1
k8w <twoeo@qq.com>
2019-01-19 22:21:36 +0800

    final

--- assets/scripts/Input.ts
+++ assets/scripts/Input.ts
@@ -2,37 +2,12 @@ const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class Input extends cc.Component {
-    onEnable() {
-        // this._addDom();
-    }
 
-    private _addDom() {
-//         let div = document.createElement('div');
-//         div.id = 'input';
-//         Object.assign(div.style, {
-//             position: 'fixed',
-//             left: 0,
-//             top: 0,
-//             bottom: 0,
-//             right: 0,
-//             display: 'flex',
-//             flexDirection: 'column',
-//             textAlign: 'center',
-//             justifyContent: 'center'
-//         })
-//         div.innerHTML = `
-// <p>请输入姓名</p>
-// <p><input type="text" /></p>
-// <p>请选择入职时间</p>
-// <p><input type="date" /></p>   
-//         `.trim();
-//         document.body.appendChild(div);
+    onEnable() {
+        document.getElementById('input')!.style.display = 'flex';
     }
 
     onDisable() {
-        // let input = document.getElementById('input');
-        // if (input) {
-        //     input.remove();
-        // }
+        document.getElementById('input')!.style.display = 'none';
     }
 }
--- assets/scripts/Main.ts
+++ assets/scripts/Main.ts
@@ -1,16 +1,95 @@
-// Learn TypeScript:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/typescript.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
-// Learn Attribute:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+import TextConfig from "./TextConfig";
 
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 @ccclass
-export default class NewClass extends cc.Component {
+export default class Main extends cc.Component {
+
+    @property(cc.Node)
+    result: cc.Node = null as any;
+
+    @property(cc.Node)
+    input: cc.Node = null as any;
+
+    @property(cc.Label)
+    text1: cc.Label = null as any;
+
+    @property(cc.Label)
+    text2: cc.Label = null as any;
+
+    @property(cc.Label)
+    keyword: cc.Label = null as any;
+
+    onLoad() {
+        (window as any).showResult = this.showResult;
+
+        // TEST
+        // this.showResult('王忠阳', new Date('2019/01/17'));
+    }
+
+    showResult = async (name: string, date: Date) => {
+        let minYear = TextConfig.min(v => parseInt(v.year))!;
+        let maxYear = TextConfig.max(v => parseInt(v.year))!;
+        let year = Math.max(Math.min(date.getFullYear(), maxYear), minYear) + '';
+
+        let config = TextConfig.find(v => v.year == year);
+        if (!config) {
+            alert('入职时间填写错误');
+            return;
+        }
+
+        console.log('showResult')
+        this.result.children.forEach(v => {
+            v.active = false;
+        })
+
+        this.input.active = false;
+        await this._wait(200);
+        this.result.active = true;
+
+        let day = ((Date.today() - date.getTime()) / 86400000 | 0) + 1;
+        this.text1.string = `亲爱的${name}
+感谢您与北汽新能源共同成长的
+${day}个日日夜夜`;
+
+        this.text2.string = config.words[Math.random() * config.words.length | 0];
+        this.keyword.string = `${year} ${config.keyword}`;
+
+        const gap = 2;
+        const duration = 1;
+        this.result.children.forEach((v, i) => {
+            v.active = true;
+            v.opacity = 0;
+            v.scale = 0.9;
+            v.runAction(cc.sequence(
+                cc.delayTime(gap * i),
+                cc.fadeIn(duration)
+            ))
+        });
+
+        setTimeout(() => {
+            cc.director.once(cc.Director.EVENT_AFTER_DRAW, () => {
+                let dataUrl = cc.game.canvas.toDataURL();
+
+                let img = document.createElement('img');
+                Object.assign(img.style, {
+                    position: 'fixed',
+                    left: 0,
+                    top: 0,
+                    width: '100vw',
+                    height: '100vh',
+                    opacity: 0
+                })
+                document.body.appendChild(img);
+                img.src = dataUrl;
+            })
+        }, ((this.result.children.length - 1) * gap + duration) * 1000 + 200);
+    }
+
+    private async _wait(ms: number) {
+        return new Promise(rs => {
+            setTimeout(() => { rs() }, ms)
+        })
+    }
 
 }
